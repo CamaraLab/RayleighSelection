664926440fb6bacc0fd6a541b86e25b6ae2db862
Seshu Yamajala <syamajala@gmail.com>
2018-02-16 16:20:26 -0500

    Add vr_complex function.

---
+++ R/vr_complex.R
@@ -0,0 +1,17 @@
+vr_complex <- function(dist, epsilon, weight = TRUE, t = Inf)
+{
+  idx <- (dist <= epsilon)
+
+  if (!weight)
+  {
+    weight <- 'NULL'
+  }
+
+  adjacency <- idx*dist
+  adjacency <- exp(-adjacency**2/t)*idx
+
+  g2 <- graph.adjacency(adjacency, mode='undirected', weighted=weight)
+  g2$adjacency <- get.adjacency(g2, sparse = TRUE)
+  class(g2) <-  c('simplicial', 'igraph')
+  return(g2)
+}

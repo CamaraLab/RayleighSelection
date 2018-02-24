0af240d8f897a8f757424557bfab5a30031bbfe4
Seshu Yamajala <syamajala@gmail.com>
2018-02-24 11:13:29 -0500

    Add knn complex.

---
+++ R/knn_complex.R
@@ -0,0 +1,20 @@
+library(dbscan)
+
+knn_complex <- function(dist, k, t = Inf)
+{
+  kneighbors <- kNN(dist, k)
+  id <- kneighbors$id
+  nodes <- dim(id)[1]
+  rows <- 1:nodes
+  adjacency <- matrix(0, nrow=nodes, ncol=nodes)
+
+  for (col in 1:k)
+  {
+    adjacency[cbind(rows, col)] <- 1
+    adjacency[cbind(col, rows)] <- 1
+  }
+
+  adjacency <- exp(-dist**2/t)*adjacency
+
+  return (graph_to_complex(adjacency))
+}
